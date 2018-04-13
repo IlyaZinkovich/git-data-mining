7d00e10a935e8612c0891488995333e5f9906390
Ilya Zinkovich <ilya.zinkovich@gmail.com>
2018-04-14 00:42:46 +0200

    Updated query with additional clause

--- src/main/java/io/coupling/git/data/mining/analysis/Query.java
+++ src/main/java/io/coupling/git/data/mining/analysis/Query.java
@@ -2,9 +2,12 @@ package io.coupling.git.data.mining.analysis;
 
 public class Query {
 
-  private static final String query = "MATCH (file:ChangedFile)-[r:CHANGED_TOGETHER_WITH]->()\n"
-      + "WHERE file.path ENDS WITH '.java' AND r.timestamp>='2017-01-01'\n"
-      + "WITH file, count(r) as togethersCount\n"
-      + "ORDER BY togethersCount DESC\n"
-      + "RETURN file, togethersCount LIMIT 10";
+  private static final String query =
+      "MATCH (file:ChangedFile)-[r:CHANGED_TOGETHER_WITH]->(anotherFile:ChangedFile)\n"
+          + "WHERE file.path ENDS WITH '.java'\n"
+          + "AND anotherFile.path ENDS WITH '.java'\n"
+          + "AND r.timestamp>='2017-01-01'\n"
+          + "WITH file, count(r) as togethersCount\n"
+          + "ORDER BY togethersCount DESC\n"
+          + "RETURN file, togethersCount LIMIT 10";
 }
